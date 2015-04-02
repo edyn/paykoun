1de304519f4cc89c817ed72d48eadb497520708b
Paul Cowgill <pauldavidcowgill@gmail.com>
2015-04-02 16:33:16 -0700

    Typo fixes.

--- docs/create_worker_spec.js
+++ docs/create_worker_spec.js
@@ -2,19 +2,19 @@ exports = Paykoun.createWorker('UniqueWorkerName', {
 
   /**
   * Declare a list of events that should trigger this worker
-  * The specific event will be available inside the 'job' object as : job.eventName
+  * The specific event(s) will be available inside the 'job' object as : job.eventName
   * 
   * @see work
   * @see recurrence
   *<
-  * @todo How are we going to deal with events params discrepancy  
+  * @todo How are we going to deal with events-params discrepancy  
   */
-  triggers: function(){ // This can also be directly a array or a string instead of a function
+  triggers: function(){ // This can also be an array or a string instead of a function
     return ['eventName1', 'eventName2'];
   },
 
   /*
-  * This declare the recurence of the job it it is a recurrent job.
+  * This declare the recurence of the job if it is a recurrent job.
   * Should not be declared if the job should only run when triggered by an event.
   * @see 'triggers'
   */
@@ -25,11 +25,11 @@ exports = Paykoun.createWorker('UniqueWorkerName', {
 
 
   /*
-  * This fonction is triggered whenever a new job has to be done
+  * This function is triggered whenever a new job has to be done
   * 
   * @job  An object containing the job to be done. Contains any metadata needed
-  * @done a callback to be ran when we are done doing the job
-  *       This param is optional, if the fonction doesn't declare this param, it is assumed
+  * @done a callback to be run when we are done doing the job
+  *       This param is optional, if the function doesn't declare this param, it is assumed
   *       to be a syncronous function and will be run on the declared thread pool
   *
   * @return
@@ -49,8 +49,8 @@ exports = Paykoun.createWorker('UniqueWorkerName', {
   * This indicate the context in which we want the worker to run.
   *
   * The goal of isolation is to prevent a specific worker to take up more resources than it should
-  * We will be able to specify the ressources we want a worker to take from a dynamicaly.
-  * In this way, whenever a specifc part of our system is buggy or failing we will be able to point to it.
+  * We will be able to specify the resources we want a worker to take dynamically.
+  * In this way, whenever a specific part of our system is buggy or failing we will be able to point to it.
   * We can also rest assured that a single component of our app will never take the whole service down.
   *
   * One of the important resource is CPU (that we can map to a timeout).
