f5153e98f4deb935a3e7aec67402f4577dd4d1a1
Diallo Mamadou Bobo <exilire2vie@gmail.com>
2015-04-01 17:09:39 -0700

    Integrated Mocha, Sinon, chai

---
+++ test/lib/paykoun.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var paykounPath = '../../lib/paykoun';
+
+
+var chai = require('chai'); // assertion library
+var expect = chai.expect;
+var should = chai.should;
+var sinon = require('sinon');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+
+var Paykoun = require(paykounPath);
+
+describe('Paykoun', function(){
+
+  it('Should run the test', function(){
+  });
+});
